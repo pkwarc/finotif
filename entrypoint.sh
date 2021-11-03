5396476f7ac2a6ae0f043c75e3e4072332d86bf8
Piotr <piotr.j.kwarcinski@gmail.com>
2021-11-03 19:50:08 +0100

    Run tests from entrypoint.sh, add dockerhub push stage

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,34 @@
 #! /bin/bash
 
+Cyan='\e[36m'
+Red='\e[31m'
 
-# In real production run this manually
-# --no-input (answer "yes" to everything)
-python manage.py collectstatic --no-input
-python manage.py makemigrations notifications --no-input
-python manage.py migrate --no-input
+printf $Red"*******************************\n"
+printf $Red"*******************************\n"
+printf $Red"Starting Production Enviroment\n"
+printf $Red"*******************************\n"
+printf $Red"*******************************\n"
 
-gunicorn --bind 0.0.0.0:$APP_PORT \
-  --capture-output                \
-  --access-logfile '-'            \
-  --errror-logfile '-'            \
-  config.wsgi:application
+
+if [ "$1" = "runserver" ]; then
+  # In real production run this manually
+  # --no-input (answer "yes" to everything)
+  python manage.py collectstatic --no-input
+  python manage.py makemigrations notifications --no-input
+  python manage.py migrate --no-input
+
+
+  gunicorn --bind 0.0.0.0:$APP_PORT \
+    --capture-output                \
+    --access-logfile '-'            \
+    --errror-logfile '-'            \
+    config.wsgi:application
+elif [ "$1" = 'test' ]; then
+    pytest
+elif [ "$1" = 'test' ]; then
+  python manage.py "${@:2}"
+else
+  exec "$@"
+fi
+
+exit
